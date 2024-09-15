cdf4740c16fc24dd9023cb0fec2e0e6b611c3697
Shadrack Ankomahene <ankomaheneshadrack@gmail.com>
2024-09-15 12:04:41 +0000

    add page to search for specific product and product in category

--- src/app/(root-layout)/services/print/categories/[categoryId]/search/[tag]/page.tsx
+++ src/app/(root-layout)/services/print/categories/[categoryId]/search/[tag]/page.tsx
@@ -3,66 +3,53 @@ import { ProductType } from "@/features/services/ProductType";
 import { formatString } from "@/functions";
 import { redirectAdminUser } from "@/lib/actions/admin-check.actions";
 import { client } from "@/lib/client";
-import { printProductsByTypeQuery } from "@/queries/products";
+import { allProductsInCategoryByTagQuery } from "@/queries/products";
 import { IPrintProduct } from "@/types";
-import { Text } from "@mantine/core";
 import { Metadata } from "next";
-import Link from "next/link";
-import React from "react";
-
-export const revalidate = 0;
 
 interface Props {
   params: {
     categoryId: string;
     productType: string;
+    tag: string;
   };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
-    title: formatString(params.productType) + " | FMT Design and Print",
+    title: `${formatString(params.tag)} ${params.categoryId} | FMT Design and Print`,
   };
 }
 
-const ProductTypePage = async ({ params }: Props) => {
+const CodingShirtsPage = async ({ params }: Props) => {
   await redirectAdminUser();
 
   const products: IPrintProduct[] = await client.fetch(
-    printProductsByTypeQuery,
+    allProductsInCategoryByTagQuery,
     {
-      slug: params.productType,
+      slug: params.categoryId,
+      itemTag: formatString(params.tag),
     }
   );
 
   const items = [
     { title: "Printing Services", href: "/services?st=print" },
     {
-      title: products[0]?.category.title,
+      title: products[0]?.category.title || params.categoryId,
       href: `/services/print/categories/${params.categoryId}`,
     },
     {
-      title: products[0]?.type.title,
-      href: `/services/print/categories/${params.categoryId}/${params.productType}`,
+      title: params.tag,
+      href: ``,
     },
   ];
 
   return (
     <div>
       <BreadcrumbRenderer items={items} />
-      <Text ta="right" mx="xl" size="sm" py="md">
-        Click{" "}
-        <Link href={`/custom-request/${params.productType}`}>
-          <Text component="span" c="pink">
-            here
-          </Text>
-        </Link>{" "}
-        to make custom {params.productType} print request.
-      </Text>
-
       <ProductType products={products} />
     </div>
   );
 };
 
-export default ProductTypePage;
+export default CodingShirtsPage;
