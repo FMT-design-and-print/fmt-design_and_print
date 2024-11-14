bfa043071448c9f25b325bc97497c47a7f442d2b
Shadrack Ankomahene <ankomaheneshadrack@gmail.com>
2024-11-14 23:53:36 +0000

    remove sanity client fetch

--- src/app/(root-layout)/page.tsx
+++ src/app/(root-layout)/page.tsx
@@ -1,21 +1,28 @@
 import { Landing } from "@/features/landing";
-import { IFeaturedProducts } from "@/types";
+// import { IFeaturedProducts } from "@/types";
 import { redirectAdminUser } from "@/lib/actions/admin-check.actions";
-import { client } from "@/lib/client";
-import { featuredProductsQuery } from "@/queries";
+// import { client } from "@/lib/client";
+// import { featuredProductsQuery } from "@/queries";
+
+const defaultFeaturedProducts = {
+  tShirts: [],
+  hoodies: [],
+  mugs: [],
+  frames: [],
+};
 
 export const revalidate = 0;
 
 export default async function Home() {
   await redirectAdminUser();
 
-  const featuredProducts: IFeaturedProducts[] = await client.fetch(
-    featuredProductsQuery
-  );
+  // const featuredProducts: IFeaturedProducts[] = await client.fetch(
+  //   featuredProductsQuery
+  // );
 
   return (
     <>
-      <Landing featuredProducts={featuredProducts[0]} />
+      <Landing featuredProducts={defaultFeaturedProducts} />
     </>
   );
 }
