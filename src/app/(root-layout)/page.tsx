b4f4c9b8dac878733a73c329f1ffc6d80980708e
Shadrack Ankomahene <ankomaheneshadrack@gmail.com>
2024-11-19 23:45:01 +0000

    loading featured products on the client

--- src/app/(root-layout)/page.tsx
+++ src/app/(root-layout)/page.tsx
@@ -1,25 +1,14 @@
 import { Landing } from "@/features/landing";
 import { redirectAdminUser } from "@/lib/actions/admin-check.actions";
-import { client } from "@/lib/client";
-import { featuredProductsQuery } from "@/queries";
-import { IFeaturedProducts } from "@/types";
 
 export const revalidate = 0;
 
 export default async function Home() {
   await redirectAdminUser();
-  let featuredProducts: IFeaturedProducts[] = [];
-
-  try {
-    featuredProducts = await client.fetch(featuredProductsQuery);
-    console.log(featuredProducts);
-  } catch (error) {
-    console.error(error);
-  }
 
   return (
     <>
-      <Landing featuredProducts={featuredProducts[0]} />
+      <Landing />
     </>
   );
 }
